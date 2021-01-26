ca67b14b0b6afc575eab03fec89ca5293ffb68d9
David Jarillo <vjdavid621@gmail.com>
2021-01-26 12:12:30 -0800

    adding routes of courses and teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  jsonapi_resources :courses
+  jsonapi_resources :teachers
 end
